28048796e1f370b71e12d85c27eba168de5619d9
Pradeva <90541409+Pradeva@users.noreply.github.com>
2025-06-26 08:13:07 +0700

    update experiences

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,15 +1,24 @@
 export const experiences = [
   {
     id: 1,
+    title: 'IT Development Program',
+    company: "PT Bank Pembangunan Daerah Jawa Barat dan Banten Tbk",
+    duration: "(Feb 2025 - Present)",
+    description1: "- Participated in an intensive training program covering key areas of banking IT, including software engineering, database management, and cybersecurity.",
+    description2: "- Involved in the development of a SLIK summarization system to support the needs of the Commercial Division.",
+    description3: "- Collaborated with cross-functional teams to analyze business requirements and implement scalable, efficient tech solutions."
+  },
+  {
+    id: 2,
     title: 'Computer Vision Engineer',
     company: "Rastek Inovasi Digital",
-    duration: "(Dec 2023 - Present)",
+    duration: "(Dec 2023 - Feb 2025)",
     description1: "- Suicide prevention detection on the Jakarta Pedestrian Bridge.",
     description2: "- Developed a Video Management System website based on Node Js and React.",
     description3: "- Jababeka area surveillance detection (as Computer Vision division lead). Using Intel Geti and Yolo for the Computer Vision detection model and Python as the programming language."
   },
   {
-    id: 2,
+    id: 3,
     title: "FullStack and Mobile Developer",
     company: "Rastek Inovasi Digital",
     duration: "(Jul 2023 - Dec 2023)",
@@ -18,10 +27,10 @@ export const experiences = [
     description3: "- Using Javascript (node.js) as the programming language, Express Sequelize for the backend framework, React Js for the frontend framework, and Flutter and React Native for the mobile development framework.",
   },
   {
-    id: 3,
+    id: 4,
     title: "FullStack and Mobile Developer",
     company: "Freelance",
-    duration: "(Aug 2022 - Present)",
+    duration: "(Aug 2022 - Feb 2025)",
     description1: "- Development of an integrated office management mobile application with an automated attendance system using node.js, Express, and Flutter.",
     description2: "- Development of ticket sales and live stream event websites for concerts and online seminars using node.js, Express, Sequelize, and React Js.",
     description3: "- Development of a video management system for all camera brands using RTSP access with node.js, Express, and React Js."
